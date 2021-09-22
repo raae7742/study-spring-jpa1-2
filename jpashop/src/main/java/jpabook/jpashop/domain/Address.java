dac1f9968a4425edde6a5826b95714ccfd3461b7
Hyeonae Jang <aeae_1@naver.com>
2021-09-22 15:44:33 +0900

    section 3-6: 회원, 상품, 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,16 +1,20 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)             // 좀 더 최적화됨 - 읽기만 실행
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional                              // default(readOnly = false)
+    public Long join(Member member) {
+        validateDuplicateMember(member);        // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // id로 회원 조회
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
